f1205148d205bd86511f6ae92c2f868dfe5c019d
JsebastianUVPRQ <penaloza.juan@correounivalle.edu.co>
2024-05-17 00:00:37 -0500

    New_file

---
+++ 2_second_highest_salary.sql
@@ -0,0 +1,16 @@
+// +-------------+------+
+// | Column Name | Type |
+// +-------------+------+
+// | id          | int  |
+// | salary      | int  |
+// +-------------+------+
+// id is the primary key (column with unique values) for this table.
+// Each row of this table contains information about the salary of an employee.
+//  
+// 
+// Write a solution to find the second highest salary from the Employee table. If there is no second highest salary, return null (return None in Pandas).
+
+select max(salary) as SecondHighestSalary
+from Employee
+where salary < (select max(salary) from Employee);
+ 
