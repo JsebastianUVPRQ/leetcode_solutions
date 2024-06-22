53b60f623e57d52c4d4a4ad7d973864afc57a706
JsebastianUVPRQ <penaloza.juan@correounivalle.edu.co>
2024-06-21 23:55:05 -0500

    modified:   2_friend_requests_II.sql

--- 2_friend_requests_II.sql
+++ 2_friend_requests_II.sql
@@ -1,4 +1,4 @@
--- 
+-- ---------------------MySql-----------------------
 WITH 
 cte1 AS (
     SELECT requester_id AS id, COUNT(*) AS num
@@ -20,3 +20,51 @@ SELECT id, SUM(num) AS num
 FROM cte3
 GROUP BY id
 ORDER BY num DESC
+
+-- -----------------------------------oracle ---------------------
+WITH CombinedFriends AS (
+    SELECT requester_id AS id, accepter_id AS friend_id
+    FROM RequestAccepted
+    UNION ALL
+    SELECT accepter_id AS id, requester_id AS friend_id
+    FROM RequestAccepted
+)
+SELECT * FROM CombinedFriends;
+
+WITH CombinedFriends AS (
+    SELECT requester_id AS id, accepter_id AS friend_id
+    FROM RequestAccepted
+    UNION ALL
+    SELECT accepter_id AS id, requester_id AS friend_id
+    FROM RequestAccepted
+),
+FriendCounts AS (
+    SELECT id, COUNT(DISTINCT friend_id) AS num
+    FROM CombinedFriends
+    GROUP BY id
+)
+SELECT * FROM FriendCounts ORDER BY num DESC;
+
+WITH CombinedFriends AS (
+    SELECT requester_id AS id, accepter_id AS friend_id
+    FROM RequestAccepted
+    UNION ALL
+    SELECT accepter_id AS id, requester_id AS friend_id
+    FROM RequestAccepted
+),
+FriendCounts AS (
+    SELECT id, COUNT(DISTINCT friend_id) AS num
+    FROM CombinedFriends
+    GROUP BY id
+),
+RankedFriends AS (
+    SELECT id, num
+    FROM FriendCounts
+    ORDER BY num DESC
+)
+SELECT id, num
+FROM (
+    SELECT id, num, ROWNUM AS r
+    FROM RankedFriends
+)
+WHERE r = 1;
