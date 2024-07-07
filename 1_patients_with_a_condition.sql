e1b6e1c2d9937bcfb6408501dc1d4b110ed824dd
JsebastianUVPRQ <penaloza.juan@correounivalle.edu.co>
2024-07-07 00:34:23 -0500

    Update 1_patients_with_a_condition.sql

--- 1_patients_with_a_condition.sql
+++ 1_patients_with_a_condition.sql
@@ -88,15 +88,8 @@ Return the result table in any order.
 
 The result format is in the following example.
 */
-
-SELECT 
-    u.product_id, 
-    COALESCE(ROUND(SUM(p.price * u.units) / (SUM(u.units), 2), 0) AS average_price
-FROM 
-    Prices p
- JOIN 
-    UnitsSold u ON p.product_id = u.product_id
-WHERE 
-    u.purchase_date BETWEEN start_date AND end_date
-GROUP BY 
-    u.product_id;
+select p.product_id, round(coalesce(sum(p.price*u.units)/nullif(sum(u.units),0),0),2) as average_price
+from Prices p
+left join UnitsSold u
+on p.product_id = u.product_id and u.purchase_date between p.start_date and p.end_date
+group by p.product_id;
