3c9d733aeb9633eb6536f7e454150544e3238d78
JsebastianUVPRQ <penaloza.juan@correounivalle.edu.co>
2024-07-07 00:19:00 -0500

    Update 1_patients_with_a_condition.sql

--- 1_patients_with_a_condition.sql
+++ 1_patients_with_a_condition.sql
@@ -53,3 +53,40 @@ where conditionS like '% DIAB1%' OR conditionS like 'DIAB1%';
 /* Write your PL/SQL query statement below */
 select * from patients
 where conditionS like '% DIAB1%' OR conditionS like 'DIAB1%' ;
+
+/* --------------- Average Selling Price ---------------
+Table: Prices
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| product_id    | int     |
+| start_date    | date    |
+| end_date      | date    |
+| price         | int     |
++---------------+---------+
+(product_id, start_date, end_date) is the primary key (combination of columns with unique values) for this table.
+Each row of this table indicates the price of the product_id in the period from start_date to end_date.
+For each product_id there will be no two overlapping periods. That means there will be no two intersecting periods for the same product_id.
+ 
+
+Table: UnitsSold
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| product_id    | int     |
+| purchase_date | date    |
+| units         | int     |
++---------------+---------+
+This table may contain duplicate rows.
+Each row of this table indicates the date, units, and product_id of each product sold.
+*/
+select product_id, start_date, end_date, price, units from Prices p
+join UnitsSold u
+on p.product_id = u.product_id
+and u.purchase_date between p.start_date and p.end_date
+order by product_id, purchase_date;
+
+
+
