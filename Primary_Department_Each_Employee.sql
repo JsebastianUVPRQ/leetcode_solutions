5e8755af035335d3833ff3dbc69f90bb03f5effe
JsebastianUVPRQ <penaloza.juan@correounivalle.edu.co>
2024-07-08 23:48:50 -0500

    Update Primary_Department_Each_Employee.sql

--- Primary_Department_Each_Employee.sql
+++ Primary_Department_Each_Employee.sql
@@ -63,6 +63,12 @@ Both quality and poor_query_percentage should be rounded to 2 decimal places.
 Return the result table in any order.
 */
 
-select query_name, round(avg(cast(rating as float)/position), 2) as quality, (round(sum(case when rating < 3 then 1 else 0 end)/count(*), 4))*100 as poor_query_percentage
-from Queries
-group by query_name
+SELECT 
+    query_name,
+    ROUND(AVG(rating/position), 2) AS quality,
+    ROUND(SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END) / COUNT(*) * 100, 2) AS poor_query_percentage
+FROM 
+    Queries
+WHERE query_name is not null
+GROUP BY
+    query_name;
