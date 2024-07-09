93c41fb5e22a170721c1d5fc0220ca768283f1f3
JsebastianUVPRQ <penaloza.juan@correounivalle.edu.co>
2024-07-09 00:38:50 -0500

    Update Primary_Department_Each_Employee.sql

--- Primary_Department_Each_Employee.sql
+++ Primary_Department_Each_Employee.sql
@@ -122,3 +122,45 @@ select id, movie, description, rating
 from Cinema
 where id % 2 = 1 and description != 'boring'
 order by rating desc
+
+
+/* ----------- Pick Names in a Table -----------
+Table: Users
+
++----------------+---------+
+| Column Name    | Type    |
++----------------+---------+
+| user_id        | int     |
+| name           | varchar |
++----------------+---------+
+user_id is the primary key (column with unique values) for this table.
+This table contains the ID and the name of the user. The name consists of only lowercase and uppercase characters.
+ 
+
+Write a solution to fix the names so that only the first character is uppercase and the rest are lowercase.
+
+Return the result table ordered by user_id.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Users table:
++---------+-------+
+| user_id | name  |
++---------+-------+
+| 1       | aLice |
+| 2       | bOB   |
++---------+-------+
+Output: 
++---------+-------+
+| user_id | name  |
++---------+-------+
+| 1       | Alice |
+| 2       | Bob   |
++---------+-------+
+*/
+
