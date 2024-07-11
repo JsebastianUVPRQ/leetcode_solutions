0b5a301c155f0d27a886b6773299a4205bcafa33
JsebastianUVPRQ <penaloza.juan@correounivalle.edu.co>
2024-07-11 00:48:11 -0500

    Update lastperson_fits_bus.sql

--- lastperson_fits_bus.sql
+++ lastperson_fits_bus.sql
@@ -24,12 +24,15 @@ The result format is in the following example.
 
 
 */
-
-SELECT person_name
-FROM Queue
-GROUP BY person_id
-HAVING SUM(weight) <= 1000
-ORDER BY person_id DESC
-LIMIT 1;
-
-
+# Write your MySQL query statement below
+with recursive cte as(
+    select person_name,turn,weight as total
+    from queue
+    where turn=1
+    union all
+    select q.person_name,q.turn,q.weight+cte.total as total
+    from queue q
+    join cte on cte.turn+1=q.turn
+    where cte.total+q.weight<=1000
+)
+select person_name from cte order by total desc limit 1
