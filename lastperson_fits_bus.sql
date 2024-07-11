b896296fffbfb0230da45ddbdb209d5d453f29b3
JsebastianUVPRQ <penaloza.juan@correounivalle.edu.co>
2024-07-11 00:53:09 -0500

    Update lastperson_fits_bus.sql

--- lastperson_fits_bus.sql
+++ lastperson_fits_bus.sql
@@ -36,3 +36,27 @@ with recursive cte as(
     where cte.total+q.weight<=1000
 )
 select person_name from cte order by total desc limit 1
+
+/*
+manager with at least 5 direct reports
+Table: Employee
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
+| department  | varchar |
+| managerId   | int     |
++-------------+---------+
+id is the primary key (column with unique values) for this table.
+Each row of this table indicates the name of an employee, their department, and the id of their manager.
+If managerId is null, then the employee does not have a manager.
+No employee will be the manager of themself.
+ 
+
+Write a solution to find managers with at least five direct reports.
+
+Return the result table in any order.
+
+*/
