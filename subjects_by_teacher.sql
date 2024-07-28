a610ef36268ceb328cb67d5e861e9337f4721754
JsebastianUVPRQ <penaloza.juan@correounivalle.edu.co>
2024-07-27 23:57:14 -0500

    Update subjects_by_teacher.sql

--- subjects_by_teacher.sql
+++ subjects_by_teacher.sql
@@ -1,27 +1,31 @@
-## +-------------+------+
-## | Column Name | Type |
-## +-------------+------+
-## | teacher_id  | int  |
-## | subject_id  | int  |
-## | dept_id     | int  |
-## +-------------+------+
-## (subject_id, dept_id) is the primary key (combinations of columns with unique values) of this table.
-## Each row in this table indicates that the teacher with teacher_id teaches the subject subject_id in the department dept_id.
-##  
-## 
-## Write a solution to calculate the number of unique subjects each teacher teaches in the university.
-## 
-## Return the result table in any order.
-## 
-## The result format is shown in the following example.
+ /*
+ +-------------+------+
+ | Column Name | Type |
+ +-------------+------+
+ | teacher_id  | int  |
+ | subject_id  | int  |
+ | dept_id     | int  |
+ +-------------+------+
+ (subject_id, dept_id) is the primary key (combinations of columns with unique values) of this table.
+ Each row in this table indicates that the teacher with teacher_id teaches the subject subject_id in the department dept_id.
+  
+ 
+ Write a solution to calculate the number of unique subjects each teacher teaches in the university.
+ 
+ Return the result table in any order.
+ 
+ The result format is shown in the following example.
 
 Select teacher_id, count(distinct subject_id) as subject_count
 from subjects_by_teacher
 group by teacher_id
+*/
+
 -- -------------------------------------------------------
 
 -- -------------------------------------------------- Replace Employee ID With The Unique Identifier --------------------------------------
--- -------------------------------------------------------
+
+/* -------------------------------------------------------
 Table: Employees
 
 +---------------+---------+
@@ -47,6 +51,7 @@ Each row of this table contains the id and the corresponding unique id of an emp
  
 
 Write a solution to show the unique ID of each user, If a user does not have a unique ID replace just show null.
+*/
 
 SELECT
 EmployeeUNI.unique_id, Employees.name
