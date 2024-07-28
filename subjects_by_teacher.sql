dd99efa5dae7619a6b7333a3b7eb18a628743cbd
JsebastianUVPRQ <penaloza.juan@correounivalle.edu.co>
2024-07-27 23:58:34 -0500

    Update subjects_by_teacher.sql

--- subjects_by_teacher.sql
+++ subjects_by_teacher.sql
@@ -53,7 +53,6 @@ Each row of this table contains the id and the corresponding unique id of an emp
 Write a solution to show the unique ID of each user, If a user does not have a unique ID replace just show null.
 */
 
-SELECT
-EmployeeUNI.unique_id, Employees.name
+SELECT EmployeeUNI.unique_id, Employees.name
 FROM Employees
 LEFT JOIN EmployeeUNI on Employees.id = EmployeeUNI.id
