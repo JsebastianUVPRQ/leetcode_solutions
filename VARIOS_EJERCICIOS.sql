cd5cf790be7e662ecb02deb6be978f8ef0dff5c3
JsebastianUVPRQ <penaloza.juan@correounivalle.edu.co>
2024-08-15 00:42:11 -0500

    Update VARIOS_EJERCICIOS.sql

--- VARIOS_EJERCICIOS.sql
+++ VARIOS_EJERCICIOS.sql
@@ -199,3 +199,8 @@ The first order of a customer is the order with the earliest order date that the
 
 Write a solution to find the percentage of immediate orders in the first orders of all customers, rounded to 2 decimal places.
 */
+
+select round(sum(case when order_date = customer_pref_delivery_date then 1 else 0 end) / count(distinct customer_id) * 100, 2) as immediate_percentage
+from Delivery
+where (customer_id, order_date) in (select customer_id, min(order_date) from Delivery group by customer_id)
+
